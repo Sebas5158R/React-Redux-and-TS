f5b004760c3148edcd71f7b8920f372ad15da477
Sebas5158R <sebastianriveraa4@gmail.com>
2024-07-29 22:12:35 -0500

    chore: Add intl-tel-input library for phone number input
    - Add "intl-tel-input" library as a new dependency in package.json
    - Update CreateNewUser component to use IntlTelInput for phone number input

--- src/components/CreateNewUser.tsx
+++ src/components/CreateNewUser.tsx
@@ -1,4 +1,6 @@
-import { Divider, TextInput } from '@tremor/react';
+import { Divider, TextInput, Select, SelectItem } from '@tremor/react';
+import IntlTelInput from "intl-tel-input/react";
+import "intl-tel-input/styles";
 
 export const CreateNewUser = () => {
 
@@ -12,7 +14,7 @@ export const CreateNewUser = () => {
           <div className="grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-6">
             <div className="col-span-full sm:col-span-3">
               <label
-                htmlFor="first-name"
+                htmlFor="full-name"
                 className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
               >
                 Nombre completo
@@ -48,7 +50,7 @@ export const CreateNewUser = () => {
             </div>
             <div className="col-span-full sm:col-span-3">
               <label
-                htmlFor="city"
+                htmlFor="country"
                 className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
               >
                 País
@@ -65,50 +67,48 @@ export const CreateNewUser = () => {
             </div>
             <div className="col-span-full sm:col-span-3">
               <label
-                htmlFor="phone"
                 className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
               >
                 Número de teléfono
                 <span className="text-red-500">*</span>
               </label>
-              <span className='flag flag-'></span>
-              <TextInput
-                type="number"
-                id="phone"
-                name="phone"
-                autoComplete="phone"
-                placeholder="Número de teléfono"
-                className="mt-2"
+              <IntlTelInput
+                initOptions={{
+                  initialCountry: "us",
+                  utilsScript: "/utils.js",
+                  showFlags: true,
+                  separateDialCode: true,
+                }}
               />
             </div>
-            <div className="col-span-full sm:col-span-2">
+            <div className="col-span-full  sm:col-span-3 mt-[9px]">
               <label
-                htmlFor="state"
+                htmlFor="document-type"
                 className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
               >
-                State
+                Tipo de documento
+                <span className="text-red-500">*</span>
               </label>
-              <TextInput
-                type="text"
-                id="state"
-                name="state"
-                autoComplete="address-level1"
-                placeholder="State"
-                className="mt-2"
-              />
+              <Select defaultValue="1" id='document-type'>
+                <SelectItem value="1">C.C</SelectItem>
+                <SelectItem value="2">P.S</SelectItem>
+                <SelectItem value="3">T.I</SelectItem>
+              </Select>
             </div>
-            <div className="col-span-full sm:col-span-2">
+
+            <div className="col-span-full sm:col-span-3">
               <label
-                htmlFor="postal-code"
+                htmlFor="document-number"
                 className="text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
               >
-                Postal code
+                Número de documento
+                <span className="text-red-500">*</span>
               </label>
               <TextInput
-                id="postal-code"
-                name="postal-code"
-                autoComplete="postal-code"
-                placeholder="Postal code"
+                id="document-number"
+                name="document-number"
+                autoComplete="document-number"
+                placeholder="Número de documento"
                 className="mt-2"
               />
             </div>
@@ -119,13 +119,13 @@ export const CreateNewUser = () => {
               type="button"
               className="whitespace-nowrap rounded-tremor-small px-4 py-2.5 text-tremor-default font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong"
             >
-              Cancel
+              Cancelar
             </button>
             <button
               type="submit"
               className="whitespace-nowrap rounded-tremor-default bg-tremor-brand px-4 py-2.5 text-tremor-default font-medium text-tremor-brand-inverted shadow-tremor-input hover:bg-tremor-brand-emphasis dark:bg-dark-tremor-brand dark:text-dark-tremor-brand-inverted dark:shadow-dark-tremor-input dark:hover:bg-dark-tremor-brand-emphasis"
             >
-              Submit
+              Guardar
             </button>
           </div>
         </form>
